54e90f290dd580f697796e9a298b9d3fe6a8e20f
saurabhsangwan <saurabhsangwan5005@gmail.com>
2017-06-07 18:23:14 +0530

    initial commit

---
+++ pickapub/config/settings/local.py
@@ -0,0 +1,15 @@
+# flake8: NOQA
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ['127.0.0.1', '0.0.0.0']
