5f6edf2fa2fb427f40ecc9e9218503d217345014
saurabhsangwan <saurabhsangwan5005@gmail.com>
2017-06-19 09:33:41 +0530

    initial -2

--- pickapub/config/settings/base.py
+++ pickapub/config/settings/base.py
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'pickapub.wsgi.application'
+WSGI_APPLICATION = 'pickapub.config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-uss'
 
 TIME_ZONE = 'Asia/Kolkata'
 
